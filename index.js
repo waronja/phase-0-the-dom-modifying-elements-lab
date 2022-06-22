3b19fe26cba333bb251ab3c7cc313bb5fc5361e5
simon waronja <simon.waronja@student.moringaschool.com>
2022-06-22 16:07:12 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+//Create new heading element with ID atttribute
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = 'Thaiya is the champion';
+
+main.appendChild(newHeader);
